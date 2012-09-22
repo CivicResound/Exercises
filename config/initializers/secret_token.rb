652bfbed39f69f0b14a41f9728a4128aa062eec7
CivicResound <joshua.micah.towns@gmail.com>
2012-09-22 18:25:03 -0400

    Initial Commit of Blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '73c9d536e0ab5a0ac109d1bbeee47ff24aec0c9d7739fa703dbb57f444de1bfe2a7c3800a8eaf444014cd8086fa5892a04b7277296b622d423b7eddf1a1c8d7f'
